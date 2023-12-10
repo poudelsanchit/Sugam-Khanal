44990d3ef098217163bc03e236740a92a4a52d3e
Sanchit poudel <snchtpdl12@gmail.com>
2023-12-10 16:03:16 +0545

    update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,13 +10,13 @@ const Home = () => {
             <ScrollContainer className="bg-bg-dark text-text-primary">
 
                 <ScrollPage >
-                    <Animator animation={batch(FadeUp)} className='w-11/12 flex flex-col l:flex-row  justify-between items-center'>
-                        <div className='w-max flex flex-col text-5xl l:text-6xl lg:text-7xl font-Roboto font-bold  justify-center items-start'>
+                    <Animator animation={batch(FadeUp)} className='w-9/12 flex flex-col l:flex-row  justify-between items-center'>
+                        <div className='w-max flex flex-col gap-10 text-5xl l:text-6xl lg:text-7xl font-Roboto font-bold  justify-center items-start'>
                             <p>HI,I'M</p>
                             <p>  SUGAM KHANAL </p>
                         </div>
                         <div className="flex flex-row w-96 justify-center">
-                            <img src={MainPhoto} alt="" className='rounded-md w-3/5' />
+                            <img src={MainPhoto} alt="" className='rounded-md w-4/5' />
                         </div>
                     </Animator>
                 </ScrollPage>
